286354470f03b7f7aac1f785074629529eb14edb
hien <xuanhienhaihau@gmail.com>
2017-12-12 18:38:42 +0700

    hien

--- Hien/test.js
+++ Hien/test.js
@@ -6,3 +6,15 @@ function myFunction() {
         x.className = "menu";
     }
 }
+
+function showtab(tabname) {
+    hiddenTabs();
+    document.getElementById("tab-" + tabname).className = "block2-1 active";
+}
+
+function hiddenTabs() {
+    var tabs = document.getElementById('tab-content').children;
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].className = "hidden";
+    }
+}
