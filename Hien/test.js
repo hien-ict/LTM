90825c0b4b2739262746a0c901fad94d82159022
hien <xuanhienhaihau@gmail.com>
2017-12-12 19:09:10 +0700

    ok1

--- Hien/test.js
+++ Hien/test.js
@@ -10,9 +10,14 @@ function myFunction() {
 function showtab(tabname) {
     hiddenTabs();
     document.getElementById("tab-" + tabname).className = "block2-1 active";
+    document.getElementById(tabname).className = tabname+" active-menu-tab";
 }
 
 function hiddenTabs() {
+    var tab = document.getElementById('menutab').children;
+    for (i = 0; i < tab.length; i++) {
+        tab[i].children[0].className = "";
+    }
     var tabs = document.getElementById('tab-content').children;
     for (i = 0; i < tabs.length; i++) {
         tabs[i].className = "hidden";
