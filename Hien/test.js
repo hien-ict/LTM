77ab9834b0dd27af5c6001b0182b13f2493dff1a
hien <xuanhienhaihau@gmail.com>
2017-12-17 11:32:04 +0700

    hien

--- Hien/test.js
+++ Hien/test.js
@@ -10,7 +10,7 @@ function myFunction() {
 function showtab(tabname) {
     hiddenTabs();
     document.getElementById("tab-" + tabname).className = "block2-1 active";
-    document.getElementById(tabname).className = tabname+" active-menu-tab";
+    document.getElementById(tabname).className = tabname + " active-menu-tab";
 }
 
 function hiddenTabs() {
@@ -23,3 +23,64 @@ function hiddenTabs() {
         tabs[i].className = "hidden";
     }
 }
+var slideIndex = 1;
+
+function plusSlides(n) {
+    clearTimeout(timer);
+    showSlides(slideIndex += n);
+    timer = setInterval("plusSlides(1)", 5000)
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("tinhot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+showSlides(slideIndex);
+timer = setInterval("plusSlides(1)", 5000)
+
+
+var slideIndex2 = 1;
+
+function plusSlides2(n) {
+    clearTimeout(timer2);
+    showSlides2(slideIndex2 += n);
+    timer2 = setInterval("plusSlides2(1)", 5000)
+}
+
+function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+    var i;
+    var slides = document.getElementsByClassName("b1");
+      var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex2 = 1
+    }
+    if (n < 1) {
+        slideIndex2 = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+    slides[slideIndex2 - 1].style.display = "block";
+      dots[slideIndex2-1].className += " active";
+}
+
+showSlides2(slideIndex2);
+timer2 = setInterval("plusSlides2(1)", 6000)
